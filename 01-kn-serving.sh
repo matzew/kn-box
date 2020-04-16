fc43bff8f84b95dc825ebdd98ef3def5058b2fc0
Matthias Wessendorf <mwessend@redhat.com>
2020-04-16 10:44:08 +0200

    more modules
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- 01-kn-serving.sh
+++ 01-kn-serving.sh
@@ -15,27 +15,15 @@ else
   reset=''
 fi
 
-kube_version="v1.18.1"
 serving_version="v0.14.0"
 kourier_version="v0.14.0"
-
-MEMORY="$(minikube config view | awk '/memory/ { print $3 }')"
-CPUS="$(minikube config view | awk '/cpus/ { print $3 }')"
-DISKSIZE="$(minikube config view | awk '/disk-size/ { print $3 }')"
-DRIVER="$(minikube config view | awk '/vm-driver/ { print $3 }')"
 
 function header_text {
   echo "$header$*$reset"
 }
 
-header_text "Starting Knative on minikube!"
-header_text "Using Kubernetes Version:               ${kube_version}"
 header_text "Using Knative Serving Version:          ${serving_version}"
 header_text "Using Kourier Version:                  ${kourier_version}"
-
-minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-8}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
-header_text "Waiting for core k8s services to initialize"
-sleep 5; while echo && kubectl get pods -n kube-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
 header_text "Setting up Knative Serving"
 
