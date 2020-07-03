6629c2203d806b22930927641c132dbf2b55a6a8
Matthias Wessendorf <mwessend@redhat.com>
2020-07-03 19:15:52 +0200

    Just Apache Kafka
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- ootb_kafka/kn-eventing-kafka.sh
+++ ootb_kafka/kn-eventing-kafka.sh
@@ -49,6 +49,7 @@ kubectl create namespace kafka
 curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
   | sed 's/namespace: .*/namespace: kafka/' \
   | kubectl -n kafka apply -f -
+sleep 5; while echo && kubectl get pods -n kafka | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
 header_text "Applying Strimzi Cluster file"
 kubectl -n kafka apply -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
@@ -83,7 +84,8 @@ kubectl patch configmap/config-network \
                "ingress.class":"kourier.ingress.networking.knative.dev"}}'
 
 header_text "Setting up Knative Eventing"
-kubectl apply --filename https://github.com/knative/eventing/releases/download/${eventing_version}/eventing.yaml
+kubectl apply --filename https://github.com/knative/eventing/releases/download/${eventing_version}/eventing-core.yaml
+kubectl apply --filename https://github.com/knative/eventing/releases/download/${eventing_version}/mt-channel-broker.yaml
 
 header_text "Waiting for Knative Eventing to become ready"
 sleep 5; while echo && kubectl get pods -n knative-eventing | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
