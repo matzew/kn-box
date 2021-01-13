ab0e6907d3a6d37a8c746d7637825861f632364b
Matthias Wessendorf <mwessend@redhat.com>
2021-01-13 15:41:59 +0100

    :dizzy: using 0.20.0 release
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- 02-kn-eventing.sh
+++ 02-kn-eventing.sh
@@ -15,7 +15,7 @@ else
   reset=''
 fi
 
-eventing_version="v0.19.4"
+eventing_version="v0.20.0"
 eventing_url=https://github.com/knative/eventing/releases/download/${eventing_version}
 
 while [[ $# -ne 0 ]]; do
