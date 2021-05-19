5d3170a1b622b107a90bc6437f4f3894d1ad92c3
Matthias Wessendorf <mwessend@redhat.com>
2021-05-19 17:37:48 +0200

    Going with Knative 0.23
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- 02-kn-eventing.sh
+++ 02-kn-eventing.sh
@@ -15,7 +15,7 @@ else
   reset=''
 fi
 
-eventing_version="v0.22.1"
+eventing_version="v0.23.0"
 eventing_url=https://github.com/knative/eventing/releases/download/${eventing_version}
 
 while [[ $# -ne 0 ]]; do
