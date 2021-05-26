fb394f1b4cffa0219ddb240244dadc56e226ba01
Matthias Wessendorf <mwessend@redhat.com>
2021-05-26 16:36:16 +0200

    Adding single strimzi
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

---
+++ 03-strimzi-single.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Turn colors in this script off by setting the NO_COLOR variable in your
+# environment to any value:
+#
+# $ NO_COLOR=1 test.sh
+NO_COLOR=${NO_COLOR:-""}
+if [ -z "$NO_COLOR" ]; then
+  header=$'\e[1;33m'
+  reset=$'\e[0m'
+else
+  header=''
+  reset=''
+fi
+
+strimzi_version=`curl https://github.com/strimzi/strimzi-kafka-operator/releases/latest |  awk -F 'tag/' '{print $2}' | awk -F '"' '{print $1}' 2>/dev/null`
+
+function header_text {
+  echo "$header$*$reset"
+}
+
+header_text "Using Strimzi Version:                  ${strimzi_version}"
+
+header_text "Strimzi install"
+kubectl create namespace kafka
+curl -L "https://github.com/strimzi/strimzi-kafka-operator/releases/download/${strimzi_version}/strimzi-cluster-operator-${strimzi_version}.yaml" \
+  | sed 's/namespace: .*/namespace: kafka/' \
+  | kubectl -n kafka create -f -
+
+# Wait for the CRD we need to actually be active
+sleep 2
+kubectl wait crd --timeout=-1s kafkas.kafka.strimzi.io --for=condition=Established
+
+header_text "Applying Strimzi Cluster file"
+###kubectl -n kafka create -f "https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/${strimzi_version}/examples/kafka/kafka-persistent-single.yaml"
+cat <<-EOF | kubectl -n kafka apply -f -
+apiVersion: kafka.strimzi.io/v1beta2
+kind: Kafka
+metadata:
+  name: my-cluster
+spec:
+  kafka:
+    version: 2.7.0
+    replicas: 1
+    listeners:
+      - name: plain
+        port: 9092
+        type: internal
+        tls: false
+      - name: external
+        port: 9094
+        tls: false
+        type: nodeport
+    config:
+      offsets.topic.replication.factor: 1
+      transaction.state.log.replication.factor: 1
+      transaction.state.log.min.isr: 1
+      log.message.format.version: "2.7"
+      inter.broker.protocol.version: "2.7"
+      auto.create.topics.enable: "true"
+    storage:
+      type: jbod
+      volumes:
+      - id: 0
+        type: persistent-claim
+        size: 100Gi
+        deleteClaim: false
+  zookeeper:
+    replicas: 1
+    storage:
+      type: persistent-claim
+      size: 100Gi
+      deleteClaim: false
+  entityOperator:
+    topicOperator: {}
+    userOperator: {}
+EOF
+
+header_text "Waiting for Strimzi to become ready"
+kubectl wait kafka --all --timeout=-1s --for=condition=Ready -n kafka
