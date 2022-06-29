21e24c744684ca32a0e8519b1f6000bc1c0dad9e
Roland Huß <rhuss@redhat.com>
2022-06-29 09:48:06 +0200

    Update eventing to 1.5.1

--- 02-kn-eventing.sh
+++ 02-kn-eventing.sh
@@ -15,7 +15,7 @@ else
   reset=''
 fi
 
-eventing_version="v1.5.0"
+eventing_version="v1.5.1"
 eventing_url=https://github.com/knative/eventing/releases/download/knative-${eventing_version}
 
 while [[ $# -ne 0 ]]; do
