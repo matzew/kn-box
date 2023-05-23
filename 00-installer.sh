ac9564087edd94e047f8b57599d110d781688a20
Matthias Wessendorf <mwessend@redhat.com>
2023-05-23 15:19:20 +0200

    Bump kube
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- 00-installer.sh
+++ 00-installer.sh
@@ -15,7 +15,7 @@ else
   reset=''
 fi
 
-kube_version="v1.23.6"
+kube_version="v1.25.9"
 
 MEMORY="$(minikube config view | awk '/memory/ { print $3 }')"
 CPUS="$(minikube config view | awk '/cpus/ { print $3 }')"
