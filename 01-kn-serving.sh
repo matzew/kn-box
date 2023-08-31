4e88d2ea54ab87401d29697b1b9a51462afd18f0
Matthias Wessendorf <mwessend@redhat.com>
2023-08-31 09:33:15 +0200

    :lipstick: Correct URL to extensions
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- 01-kn-serving.sh
+++ 01-kn-serving.sh
@@ -19,7 +19,7 @@ serving_version="v1.11.0"
 kourier_version="v1.11.1"
 
 serving_url=https://github.com/knative/serving/releases/download/knative-${serving_version}
-kourier_url=https://github.com/knative/net-kourier/releases/download/knative-${kourier_version}
+kourier_url=https://github.com/knative-extensions/net-kourier/releases/download/knative-${kourier_version}
 
 while [[ $# -ne 0 ]]; do
    parameter=$1
